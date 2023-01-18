51cd9b356f49d69287d08ed0e4521c736715cb48
森岡真由 <morioka@topic-path.com>
2023-01-18 10:45:55 +0900

    データ削除

--- docsaurus-test/website/siteConfig.js
+++
@@ -1,8 +0,0 @@
-const siteConfig = {
-    url: 'https://topicpath.github.io ',  // GitHub PagesのURL("katsube"の箇所は自分のユーザー名)
-    baseUrl: '/nd-manual/',           // GitHub Pagesのパス(projectNameと合わせる)
-  
-    projectName: 'testProject',    // サイト名
-    organizationName: 'topicpath',   // GitHubのユーザー名(または組織名)
-  }
-  
